6203e0e71c549c15ee94da04a9f3b4a23fc75ac7
Di Zhao <dizhao@Dis-MacBook-Pro.local>
2019-07-12 19:51:57 -0500

    before thrid party `

--- src/store /configureStore.js
+++ src/store /configureStore.js
@@ -1,10 +1,14 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, createStore,compose} from 'redux';
 import placesReducer from './reducers/places';
+
 const rootReducer=combineReducers({
     places:placesReducer
 });
-
+let composeEnhancers=compose;
+if(__DEV__){
+    composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose;
+}
 const configureStore=()=>{
-    return createStore(rootReducer);
+    return createStore(rootReducer,composeEnhancers());
 };
 export default configureStore;
